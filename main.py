9e7275fad27eb740750918cf081a63d9e7e2306d
Jake Mobley <jakemobley@gmail.com>
2020-11-05 17:07:37 -0600

    added file list to index.html

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import Flask, flash, request, redirect, url_for, render_template, send_file
 from werkzeug.utils import secure_filename
 from pathlib import Path
 
-UPLOAD_FOLDER = r"C:\Users\jakem\projects\web-dev\csv_atlas\csv_atlas\uploaded_files"
+FILE_DIR = r"C:\Users\jakem\projects\web-dev\csv_atlas\csv_atlas\uploaded_files"
 ALLOWED_EXTENSIONS = {'csv'}
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = FILE_DIR
 
 
 def allowed_file(filename):
@@ -15,8 +15,24 @@ def allowed_file(filename):
 
 
 @app.route('/')
-def index():
-	return render_template('index.html')
+def dir_listing():
+
+	# Joining the base and the requested path
+	abs_path = FILE_DIR
+
+	# Return 404 if path doesn't exist
+	if not os.path.exists(abs_path):
+		return abort(404, "problem with file directory")
+
+	"""
+	# Check if path is a file and serve
+	if os.path.isfile(abs_path):
+		return send_file(abs_path)
+	"""
+
+	# Show directory contents
+	files = os.listdir(abs_path)
+	return render_template('index.html', files=files)
 
 
 @app.route('/upload', methods=['GET', 'POST'])
