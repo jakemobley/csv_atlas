7dc91f8ebd9dc10011468177bcf1786f7a281c73
Jake Mobley <jakemobley@gmail.com>
2020-11-04 20:03:48 -0600

    added basic upload functionality

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+from pathlib import Path
+
+UPLOAD_FOLDER = r"C:\Users\jakem\projects\web-dev\csv_atlas\csv_atlas\uploaded_files"
+ALLOWED_EXTENSIONS = {'csv'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		# check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
+		# if user does not select file, browser also
+		# submit an empty part without filename
+		if file.filename == '':
+			flash('No selected file')
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(Path(f"{app.config['UPLOAD_FOLDER']}/{filename}"))
+			return redirect(url_for('upload_file', filename=filename))
+	return render_template('upload.html')
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
