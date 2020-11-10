659997ab151d6bac850dd592c112893fa71d53a1
Jake Mobley <jakemobley@gmail.com>
2020-11-10 17:19:02 -0600

    added error handlers

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from werkzeug.utils import secure_filename
 from pathlib import Path
 import pandas as pd
 import config
+import logging
+
+logger = logging.getLogger()
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = Path(config.FILE_DIR)
@@ -15,12 +18,21 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in config.ALLOWED_EXTENSIONS
 
 
+def directory_lookup():
+    try:
+        assert os.path.exists(config.FILE_DIR)
+    except Exception as e:
+        logger.error(e)
+        abort(412, "problem reaching file directory; check config")
+
+
 def get_page_of_data(df, offset=0, per_page=1000):
     return df.iloc[offset: offset + per_page]
 
 
 @app.route('/')
-def file_landing():
+def landing():
+    directory_lookup()
     # Show directory contents
     files = os.listdir(config.FILE_DIR)
     return render_template('file_list.html', files=files)
@@ -28,41 +40,52 @@ def file_landing():
 
 @app.route('/<file>')
 def file_list(file):
-    # Return 404 if path doesn't exist
-    if not os.path.exists(config.FILE_DIR):
-        return abort(412, "problem with file directory")
-    if file not in os.listdir(config.FILE_DIR):
-        abort(404, "file not found.")  # add custom error handlers
+    directory_lookup()
+    try:
+        assert file in os.listdir(config.FILE_DIR)
+    except Exception as e:
+        logger.error(e)
+        abort(404, "file not found in file directory.")
     return render_template('actions.html', file=file)
 
 
 @app.route('/download/<file>')
 def download_file(file):
-    # Return 404 if path doesn't exist
-    if not os.path.exists(config.FILE_DIR):
-        return abort(412, "problem with file directory")
-    if file not in os.listdir(config.FILE_DIR):
-        return redirect(url_for('file_list'))  # add custom error handlers
-    # create df to display
+    directory_lookup()
+    try:
+        assert file in os.listdir(config.FILE_DIR)
+    except Exception as e:
+        logger.error(e)
+        abort(404, "file not found in file directory")
     full_filepath = Path(f"{config.FILE_DIR}/{file}")
     return send_file(full_filepath)
 
 
 @app.route('/display/<file>')
 def display_file(file):
-    # Return 404 if path doesn't exist
-    if file not in os.listdir(config.FILE_DIR):
-        abort(404, "file not found.")  # TODO: add custom error handlers
+    directory_lookup()
+    try:
+        assert file in os.listdir(config.FILE_DIR)
+    except Exception as e:
+        logger.error(e)
+        abort(404, "file not found in file directory")
     fullpath = Path(f"{config.FILE_DIR}/{file}")
-    df = pd.read_csv(fullpath)
-    '''pagination across the nation'''
+
+    df = None
+    try:
+        df = pd.read_csv(fullpath)
+    except Exception as e:
+        logger.error(e)
+        abort(412, "Error parsing CSV file. Please check the file and try again.")
+
+    # pagination
     page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
     total = len(df)
     page_df = get_page_of_data(df, offset=offset, per_page=per_page)
     pagination = Pagination(page=page, per_page=per_page, total=total,
                             css_framework='bootstrap4')
+    # create html table from df and display
     html_df = page_df.to_html()
-
     return render_template('file.html', file=file, shape=df.shape, table=html_df, page=page, per_page=per_page,
                            pagination=pagination)
 
@@ -70,19 +93,25 @@ def display_file(file):
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
+        # check if post request has the file info
         if 'file' not in request.files:
             flash('No file part')
         file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
+
+        # flash message if no file selected
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
+
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(Path(f"{app.config['UPLOAD_FOLDER']}/{filename}"))
-            return redirect(url_for('upload_file', filename=filename))  # TODO: need success page
+            directory_lookup()
+            try:
+                file.save(Path(f"{app.config['UPLOAD_FOLDER']}/{filename}"))
+            except Exception as e:
+                logger.error(e)
+                abort(500, "Oops! Something went wrong. Please try again.")
+            return redirect(url_for("landing"))
     return render_template('upload.html')
 
 
